473955436a6e9f40f1af9f7c53b813f9f815b3cc
Alicoder001 <coderkuchkarov@gmail.com>
2023-02-14 17:25:07 +0500

    added

---
+++ precache-manifest.eb5c96bb3d22ef2cc9791f640b7a9600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0cf5a03596330bcd3905aeca2a31e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841871c2004fcd2a1b10",
+    "url": "/static/css/main.b3f701cc.chunk.css"
+  },
+  {
+    "revision": "a8fabc967e242871e0ae",
+    "url": "/static/js/2.245aba39.chunk.js"
+  },
+  {
+    "revision": "c0142ca93c9afb52e585742b27ac9f85",
+    "url": "/static/js/2.245aba39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841871c2004fcd2a1b10",
+    "url": "/static/js/main.54d95d5c.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
